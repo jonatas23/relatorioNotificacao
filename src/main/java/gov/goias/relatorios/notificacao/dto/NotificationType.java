e88527c49ddefd8cccc3e8ba8146917061471ac0
jonatas.oliveira <jonatas.oliveira@fornecedores.goias.gov.br>
2025-08-08 15:21:20 -0300

    Aprimoramento do Projeto

--- src/main/java/gov/goias/relatorios/notificacao/dto/NotificationType.java
+++ src/main/java/gov/goias/relatorios/notificacao/dto/NotificationType.java
@@ -1,8 +1,21 @@
 package gov.goias.relatorios.notificacao.dto;
 
+import lombok.Getter;
+
+@Getter
 public enum NotificationType {
-    REPORT_COMPLETED,
-    REPORT_FAILED,
-    SYSTEM_MAINTENANCE,
-    GENERAL_INFO
+    REPORT_SCHEDULED("REPORT_SCHEDULED", "Relatório Agendado"),
+    REPORT_PROCESSING("REPORT_PROCESSING", "Relatório em Processamento"),
+    REPORT_COMPLETED("REPORT_COMPLETED", "Relatório Concluído"),
+    REPORT_FAILED("REPORT_FAILED", "Falha no Relatório"),
+    REPORT_CANCELLED("REPORT_CANCELLED", "Relatório Cancelado"),
+    SYSTEM_ERROR("SYSTEM_ERROR", "Erro do Sistema");
+
+    private final String code;
+    private final String description;
+
+    NotificationType(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
